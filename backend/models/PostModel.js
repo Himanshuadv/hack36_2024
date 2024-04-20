ab8f52f8e0c0d0f20f224cb3f81878ecbf7260f4
Himanshuadv <hthimanshu739@gmail.com>
2024-04-20 14:38:11 +0530

    frontend is updated

---
+++ backend/models/PostModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+    imagePath: {
+        type: String
+      },
+  content: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+  dislikes: {
+    type: Number,
+    default: 0
+  },
+  comments: [
+    {
+      content: { type: String },
+      timestamp: { type: Date, default: Date.now },
+      user: { type: Schema.Types.ObjectId, ref: 'User' }
+    }
+  ],
+  tag: {
+    type: String
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
